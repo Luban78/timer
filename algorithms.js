e6d193d799271574aaf7c4bd8b3dabc2eba33128
Luban78 <156668846+Luban78@users.noreply.github.com>
2026-06-25 21:08:58 +0200

    Create algorithms.js

---
+++ algorithms.js
@@ -0,0 +1,28 @@
+// algorithms.js v1
+// Databáze PLL algoritmů. Později sem přidáme i OLL.
+
+export const pllAlgs={
+"Ua-perm":"R U' R U R U R U' R' U' R2",
+"Ub-perm":"R2 U R U R' U' R' U' R' U R'",
+"Z-perm":"M2 U M2 U M' U2 M2 U2 M' U2",
+"H-perm":"M2 U M2 U2 M2 U M2",
+"Aa-perm":"x R' U R' D2 R U' R' D2 R2 x'",
+"Ab-perm":"x R2 D2 R U R' D2 R U' R x'",
+"E-perm":"x' R U' R' D R U R' D' R U R' D R U' R' D'",
+"T-perm":"R U R' U' R' F R2 U' R' U' R U R' F'",
+"F-perm":"R' U' F' R U R' U' R' F R2 U' R' U' R U R' U R",
+"Ja-perm":"x R2 F R F' R U2 r' U r U2",
+"Jb-perm":"R U R' F' R U R' U' R' F R2 U' R'",
+"Ra-perm":"R U' R' U' R U R D R' U' R D' R' U2 R'",
+"Rb-perm":"R' U2 R U2 R' F R U R' U' R' F' R2",
+"V-perm":"R' U R' U' y R' F' R2 U' R' U R' F R F",
+"Y-perm":"F R U' R' U' R U R' F' R U R' U' R' F R F'",
+"Na-perm":"R U R' U R U R' F' R U R' U' R' F R2 U' R'",
+"Nb-perm":"R' U R U' R' F' U' F R U R' F R' F' R U'",
+"Ga-perm":"R2 U R' U R' U' R U' R2 D U' R' U R D'",
+"Gb-perm":"R' U' R U D' R2 U R' U R U' R U' R2 D",
+"Gc-perm":"R2 U' R U' R U R' U R2 D' U R U' R' D",
+"Gd-perm":"R U R' U' D R2 U' R U' R' U R' U R2 D'"
+};
+
+export const ollAlgs = {};
