ae4c6bf350b9899aceb10076b81ae90895195a4a
Luban78 <luban78@gmail.com>
2026-06-26 19:46:34 +0200

    update best record in history.js

--- history.js
+++ history.js
@@ -3,15 +3,18 @@ export function renderHistory(historyList, savedSolves){
     historyList.innerText = "-";
     return;
   }
+const bestTime = Math.min(...savedSolves.map(s => Number(s.time || 0)));
 
   historyList.innerHTML = savedSolves.map((s,i)=>{
     const time = Number(s.time || 0);
     const htm = s.htm ?? s.moves ?? 0;
     const tps = s.tps ?? s.avg ?? 0;
+    const isPB = time === bestTime;
+    
 
     return `
       <div class="hist-row">
-        <div class="hist-time">${i+1}. ${time.toFixed(2)}s</div>
+        <div class="hist-time">${i+1}. ${isPB ? "🥇 " : ""}${time.toFixed(2)}s</div>
         <div class="hist-small">${htm}t</div>
         <div class="hist-small">${Number(tps).toFixed(1)}TPS</div>
       </div>
