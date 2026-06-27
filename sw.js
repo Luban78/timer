8377a078b9efb2b0bbe024373dc50fbd0a114432
Luban78 <luban78@gmail.com>
2026-06-27 23:19:39 +0200

    v0.19 update service worker cache

--- sw.js
+++ sw.js
@@ -1,9 +1,61 @@
-self.addEventListener("install", e => {
+const CACHE_NAME = "mg3i-trainer-v019";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./history.js",
+  "./storage.js",
+  "./graph.js",
+  "./detailGraph.js",
+  "./sound.js",
+  "./ui.js",
+  "./algMenu.js",
+  "./cubeConnection.js",
+  "./algorithmStats.js",
+  "./algorithms.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event=>{
   self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache=>{
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(self.clients.claim());
+self.addEventListener("activate", event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>{
+      return Promise.all(
+        keys
+          .filter(key=>key!==CACHE_NAME)
+          .map(key=>caches.delete(key))
+      );
+    }).then(()=>{
+      return self.clients.claim();
+    })
+  );
 });
 
-self.addEventListener("fetch", e => {});
+self.addEventListener("fetch", event=>{
+  event.respondWith(
+    fetch(event.request)
+      .then(response=>{
+        const copy=response.clone();
+
+        caches.open(CACHE_NAME).then(cache=>{
+          cache.put(event.request,copy);
+        });
+
+        return response;
+      })
+      .catch(()=>{
+        return caches.match(event.request);
+      })
+  );
+});
